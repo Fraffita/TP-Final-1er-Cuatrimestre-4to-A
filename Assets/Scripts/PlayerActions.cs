61254c69fa867c255c719f9024e603264f1eab20
Franco Raffaele <fran.raffa15@gmail.com>
2022-06-13 09:04:24 -0300

    primera inclusión del timer (sin funcionar)

--- Assets/Scripts/PlayerActions.cs
+++ Assets/Scripts/PlayerActions.cs
@@ -5,19 +5,33 @@ using UnityEngine.UI;
 
 public class PlayerActions : MonoBehaviour
 {
-   public GameObject baseFinal;
+    public GameObject baseFinal;
     public GameObject PanelFinal;
     public Text Tiempo;
     bool tocado;
+    float timer;
+    float StartTime;
+
 
     void Start()
     {
         PanelFinal.SetActive(false);
+        StartTime = 0;
+
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            StartTime = StartTime + 1;
+
+            if (StartTime == 1)
+            {
+                Tiempo.text = timer.ToString();
+                timer = Time.deltaTime;
+            }
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -26,8 +40,12 @@ public class PlayerActions : MonoBehaviour
         {
             PanelFinal.SetActive(true);
             tocado = true;
+
         }
-
-
     }
+
+   
+
+
+
 }
